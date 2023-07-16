aeb3d0c33ba4b4e670522b56b4f78f4bbc751a97
amirack <amirack003@gmail.com>
2023-07-16 14:24:20 +0330

    Add FileReader Class - Modify gitignore

---
+++ Phase01Project1/src/FileReader.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class FileReader {
+    private static final String dataSetPath= "/Phase01Project1/SoftwareBooksDataset";
+    public Map<String,String> getFileDataWithFileName() {
+        Map<String ,String> books = new HashMap<>();
+        try {
+            Path path = Path.of("").toAbsolutePath();
+            File directoryPath = new File(path+dataSetPath);
+            File[] filesList = directoryPath.listFiles();
+            assert filesList != null;
+            for(File file : filesList) {
+                StringBuilder text = new StringBuilder();
+                Scanner myReader = new Scanner(file);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    text.append(data).append('\n');
+                }
+                myReader.close();
+                books.put(file.getName().substring(0,file.getName().length()-4), text.toString());
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return books;
+    }
+}
---
+++ Phase01Project1/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        Map<String ,String> books;
+        books = fileReader.getFileDataWithFileName();
+    }
+}
