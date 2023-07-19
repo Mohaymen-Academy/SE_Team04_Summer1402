e288ea081fc6df3b15f09a30328afad26cce9281
hosseinb <hb3238816@gmail.com>
2023-07-19 11:40:34 +0330

    Clean main

--- Phase01Project1/src/Main.java
+++ Phase01Project1/src/Main.java
@@ -2,19 +2,38 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String inputQuery = scanner.nextLine();
-        String folderPath = "/Users/hosseinb/Desktop/SE_Team04_Summer1402/Phase01Project1/SoftwareBooksDataset";
-        InvertedIndex invertedIndex = new InvertedIndex();
+
+    private final static String folderPath = "/Users/hosseinb/Desktop/SE_Team04_Summer1402/Phase01Project1/SoftwareBooksDataset";
+    private static HashSet<Document> result;
+    private static InvertedIndex invertedIndex;
+    private static String inputQuery;
+
+    public static void preprocess(){
+        invertedIndex =  new InvertedIndex();
         invertedIndex.extractDocument(folderPath);
-        HashSet<Document> result;
+    }
+
+    public static void getInput(){
+        System.out.println("Folder path : ");
+        Scanner scanner = new Scanner(System.in);
+        inputQuery = scanner.nextLine();
+    }
+
+    public static void setSearchResult(){
         result = invertedIndex.advancedSearch(inputQuery, " \n");
-        if(result.isEmpty()){
-            System.out.println("No result found");
-        }
+    }
+    public static void printSearchResult(){
         for(Document doc : result){
             System.out.println(doc.getName());
         }
+        if(result.isEmpty())
+            System.out.println("No result found");
+    }
+
+    public static void main(String[] args) {
+        preprocess();
+        getInput();
+        setSearchResult();
+        printSearchResult();
     }
 }
